bf773d2544edef94cdd49c5952da4b22f7b33244
Romantiskt <815265586@qq.com>
2022-06-06 23:03:09 +0800

    修复运行flutter 3.0.1

--- one_day_android/app/src/main/java/com/rolan/oneday/DoFlutterActivity.kt
+++ one_day_android/app/src/main/java/com/rolan/oneday/DoFlutterActivity.kt
@@ -1,7 +1,7 @@
 package com.rolan.oneday
 
 import android.os.Bundle
-import io.flutter.app.FlutterActivity
+import io.flutter.embedding.android.FlutterActivity
 
 /**
  * Created by Rolan.Wang on 4/1/21.3:18 PM
